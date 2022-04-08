88ea1127c67cfe39cd813bd6e8a9d3c140a926d4
nadysaptra <saputranady@gmail.com>
2022-04-08 10:46:04 +0700

    style: update tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ module.exports = {
   theme: {
     extend: {},
   },
+  variants: {
+    tableLayout: ['responsive', 'hover', 'focus'],
+  },
   plugins: [],
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx,html}",
-    "./public/index.html",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}', './public/index.html'],
 };
